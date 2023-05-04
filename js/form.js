34b0dc6f60fabdef4f558743bd609a7bdaad692b
Sselenso <122260649+Sselenso@users.noreply.github.com>
2023-05-04 22:51:12 +0300

    04/05_18:00

---
+++ js/form.js
@@ -0,0 +1,11 @@
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+
+const closeIcon = document.querySelector('.close-icon');
+closeIcon.addEventListener('click', closeForm);
+
